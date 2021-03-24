acb7e7d39eef918527cdcac9a62cd2fdbe1a79b8
Kas Song <kas@Kas-Alpha-MacBook-Pro.local>
2021-03-24 17:48:34 +0900

    init: Created Project & Setup Basics

---
+++ Fructus/Fructus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Kas Song on 2021/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Kas Song on 2021/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
