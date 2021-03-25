6eea7c73233ce1f3c4e8718efc5b9aa065c2045a
KasRoid <KasRoid@gmail.com>
2021-03-25 14:19:58 +0900

    feat: AppStorage & App Lifecycle

--- Fructus/Fructus/View/StartButtonView.swift
+++ Fructus/Fructus/View/StartButtonView.swift
@@ -9,10 +9,11 @@ import SwiftUI
 
 struct StartButtonView: View {
     // MARK: - Properties
+    @AppStorage("isOnboarding") var isOnboarding: Bool?
     
     // MARK: - Body
     var body: some View {
-        Button(action: { print("Exit the onboarding") },
+        Button(action: { isOnboarding = false },
                label: { HStack(spacing: 8) {
                 Text("Start")
                 Image(systemName: "arrow.right.circle")
