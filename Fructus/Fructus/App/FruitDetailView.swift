4bd61e595850c22580f640b5b9897689ed916eb9
KasRoid <KasRoid@gmail.com>
2021-03-31 17:26:41 +0900

    fix: Refactored Code

--- Fructus/Fructus/App/FruitDetailView.swift
+++ Fructus/Fructus/App/FruitDetailView.swift
@@ -15,39 +15,45 @@ struct FruitDetailView: View {
     // MARK: - Body
     var body: some View {
         NavigationView {
-            ScrollView(.vertical, showsIndicators: false, content: {
-                VStack(alignment: .center, spacing: 20, content: {
-                    FruitHeaderView(fruit: fruit)
-                    VStack(alignment: .leading, spacing: 20, content: {
-                        Text(fruit.title)
-                            .font(.largeTitle)
-                            .fontWeight(.heavy)
-                            .foregroundColor(fruit.gradientColors[0])
-                        Text(fruit.headline)
-                            .font(.headline)
-                            .multilineTextAlignment(.leading)
-                        // Nutrients
-                        FruitNutrientsView(fruit: fruit)
-                        // Subheadline
-                        Text("Learn more about \(fruit.title)".uppercased())
-                            .fontWeight(.bold)
-                            .foregroundColor(fruit.gradientColors[0])
-                        Text(fruit.description)
-                            .multilineTextAlignment(.leading)
-                        // Link
-                        SourceLinkView()
-                            .padding(.top, 10)
-                            .padding(.bottom, 40)
-                    })
-                    .padding(.horizontal, 20)
-                    .frame(maxWidth: 640, alignment: .center)
-                })
-                .navigationBarTitle(fruit.title, displayMode: .inline)
-                .navigationBarHidden(true)
-            })
-            .edgesIgnoringSafeArea(.top)
+            ScrollView(.vertical, showsIndicators: false,
+                       content: { setContentVStack() })
+                .edgesIgnoringSafeArea(.top)
         }
         .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+// MARK: - Helpers
+extension FruitDetailView {
+    func setContentVStack() -> some View {
+        VStack(alignment: .center, spacing: 20, content: {
+            FruitHeaderView(fruit: fruit)
+            VStack(alignment: .leading, spacing: 20, content: {
+                Text(fruit.title)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(fruit.gradientColors[0])
+                Text(fruit.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                // Nutrients
+                FruitNutrientsView(fruit: fruit)
+                // Subheadline
+                Text("Learn more about \(fruit.title)".uppercased())
+                    .fontWeight(.bold)
+                    .foregroundColor(fruit.gradientColors[0])
+                Text(fruit.description)
+                    .multilineTextAlignment(.leading)
+                // Link
+                SourceLinkView()
+                    .padding(.top, 10)
+                    .padding(.bottom, 40)
+            })
+            .padding(.horizontal, 20)
+            .frame(maxWidth: 640, alignment: .center)
+        })
+        .navigationBarTitle(fruit.title, displayMode: .inline)
+        .navigationBarHidden(true)
     }
 }
 
--- Fructus/Fructus/App/SettingsView.swift
+++ Fructus/Fructus/App/SettingsView.swift
@@ -18,64 +18,77 @@ struct SettingsView: View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false, content: {
                 VStack(spacing: 20) {
-                    // MARK: - Section 1
-                    GroupBox(label: HStack { SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")},
-                             content: {
-                                Divider().padding(.vertical, 4)
-                                HStack(alignment: .center) {
-                                    Image("logo")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 80, height: 80)
-                                        .cornerRadius(9)
-                                    Text("Most fruits are naturally low in fat, sodium, and calories. None have cholesterol. Fruits are sources of many essentail nutrients, including potassium, dietary fiber, vitamins, and much more.")
-                                        .font(.footnote)
-                                }
-                             })
-                    // MARK: - Section 2
-                    GroupBox(label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush"),
-                             content: {
-                                Divider().padding(.vertical, 4)
-                                Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
-                                    .padding(.vertical, 8)
-                                    .frame(minHeight: 60)
-                                    .layoutPriority(1)
-                                    .font(.footnote)
-                                    .multilineTextAlignment(.leading)
-                                
-                                Toggle(isOn: $isOnboarding, label: {
-                                    if isOnboarding {
-                                        Text("Restarted".uppercased())
-                                            .fontWeight(.bold)
-                                            .foregroundColor(Color.green)
-                                    } else {
-                                        Text("Restart".uppercased())
-                                            .fontWeight(.bold)
-                                            .foregroundColor(Color.secondary)
-                                    }
-                                })
-                                .padding()
-                                .background(Color(UIColor.tertiarySystemBackground)
-                                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous)))
-                    })
-                    // MARK: - Section 3
-                    GroupBox(label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone"),
-                             content: {
-                                SettingsRowView(name: "Developer", content: "Kas")
-                                SettingsRowView(name: "Designer", content: "Robert Petras")
-                                SettingsRowView(name: "Compatibility", content: "iOS 14")
-                                SettingsRowView(name: "Website", linkLabel: "SwiftUI Masterclass", linkDestination: "swiftuimasterclass.com")
-                                SettingsRowView(name: "Twitter", linkLabel: "@RobertPetras", linkDestination: "twitter.com/robertpetras")
-                                SettingsRowView(name: "SwiftUI", content: "2.0")
-                                SettingsRowView(name: "Version", content: "1.1.0")
-                             }
-                    )
+                    setFructusGroupBox()
+                    setCustomizationGroupBox()
+                    setApplicationGroupBox()
                 } // VStack
                 .navigationBarTitle(Text("Setting"), displayMode: .large)
                 .navigationBarItems(trailing: Button(action: { presentationMode.wrappedValue.dismiss() },
                                                      label: { Image(systemName: "xmark") }))
                 .padding()
             }) // ScrollView
+        }
+    }
+}
+
+// MARK: - Helpers
+extension SettingsView {
+    func setFructusGroupBox() -> some View {
+        return GroupBox(label: HStack { SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")},
+                        content: {
+                            Divider().padding(.vertical, 4)
+                            HStack(alignment: .center) {
+                                Image("logo")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 80, height: 80)
+                                    .cornerRadius(9)
+                                Text("Most fruits are naturally low in fat, sodium, and calories. None have cholesterol. Fruits are sources of many essentail nutrients, including potassium, dietary fiber, vitamins, and much more.")
+                                    .font(.footnote)
+                            }
+                        })
+    }
+    func setCustomizationGroupBox() -> some View {
+        return GroupBox(label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush"),
+                        content: {
+                            Divider().padding(.vertical, 4)
+                            Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                                .padding(.vertical, 8)
+                                .frame(minHeight: 60)
+                                .layoutPriority(1)
+                                .font(.footnote)
+                                .multilineTextAlignment(.leading)
+                            
+                            Toggle(isOn: $isOnboarding, label: {
+                                performToggleEffects()
+                            })
+                            .padding()
+                            .background(Color(UIColor.tertiarySystemBackground)
+                                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous)))
+                        })
+    }
+    func setApplicationGroupBox() -> some View {
+        return GroupBox(label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone"),
+                        content: {
+                            SettingsRowView(name: "Developer", content: "Kas")
+                            SettingsRowView(name: "Designer", content: "Robert Petras")
+                            SettingsRowView(name: "Compatibility", content: "iOS 14")
+                            SettingsRowView(name: "Website", linkLabel: "SwiftUI Masterclass", linkDestination: "swiftuimasterclass.com")
+                            SettingsRowView(name: "Twitter", linkLabel: "@RobertPetras", linkDestination: "twitter.com/robertpetras")
+                            SettingsRowView(name: "SwiftUI", content: "2.0")
+                            SettingsRowView(name: "Version", content: "1.1.0")
+                        }
+        )
+    }
+    func performToggleEffects() -> some View {
+        if isOnboarding {
+            return Text("Restarted".uppercased())
+                .fontWeight(.bold)
+                .foregroundColor(Color.green)
+        } else {
+            return Text("Restart".uppercased())
+                .fontWeight(.bold)
+                .foregroundColor(Color.secondary)
         }
     }
 }
